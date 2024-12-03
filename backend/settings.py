e35387c6d7314c5b89e6ec3b598087b0beac13bc
Henoker <hennybany@gmail.com>
2024-12-03 20:55:14 +0300

    add: settings config with corsheaders

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # local
+    # third party
     'rest_framework',
     'corsheaders',
 ]
@@ -45,17 +45,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # React development server
-]
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
+
 ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
