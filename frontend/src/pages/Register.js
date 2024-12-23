6e69df1fe5cc85b56e0a123f8a4094fe1b698574
Henoker <hennybany@gmail.com>
2024-12-23 21:36:15 +0300

    ADD global state using appContext and reducers

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { Logo, FormRow } from "../components";
+import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
@@ -13,6 +14,7 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
 
   // global context and useNavigate later
+  const { isLoading, showAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -32,6 +34,7 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {values.showAlert && <Alert />}
 
         {/* name field */}
         {!values.isMember && (
