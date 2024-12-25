9770d0711e86b8f38bb1a76523dd7a85b6c76294
Henoker <hennybany@gmail.com>
2024-12-25 22:00:11 +0300

    add frontend user register setup using context management

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -1,12 +1,21 @@
 import React, { useReducer, useContext } from "react";
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import axios from "axios";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
+} from "./actions";
 
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
+  user: null,
+  token: null,
 };
 const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
@@ -22,12 +31,36 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const setupUser = async ({ currentUser, endPoint, alertText }) => {
+    dispatch({ type: SETUP_USER_BEGIN });
+    try {
+      const { data } = await axios.post(
+        `/api/v1/auth/${endPoint}`,
+        currentUser
+      );
+
+      const { user, token, location } = data;
+      dispatch({
+        type: SETUP_USER_SUCCESS,
+        payload: { user, token, location, alertText },
+      });
+      // addUserToLocalStorage({ user, token, location })
+    } catch (error) {
+      dispatch({
+        type: SETUP_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         displayAlert,
         clearAlert,
+        setupUser,
       }}
     >
       {children}
