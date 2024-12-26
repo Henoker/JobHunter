ad2b533003cc1e1dd20753b7bc70f47322ac0e4b
Henoker <hennybany@gmail.com>
2024-12-26 16:53:47 +0300

    add user location field in django

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    user_location = models.CharField(max_length=255, blank=True, null=True) 
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
