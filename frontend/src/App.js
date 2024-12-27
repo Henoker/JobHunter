48b5c74c9edb21c98edce831e45f031b426bbaa7
Henoker <hennybany@gmail.com>
2024-12-27 22:05:36 +0300

    Add additional nested pages on dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import { Dashboard, Error, Landing, Register } from "./pages/Index";
+import { Error, Landing, Register } from "./pages";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/">
+          <Route path="stats" element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
