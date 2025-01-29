e7ffd176c354a51045a184b71de6de7d8a448140
Henoker <hennybany@gmail.com>
2025-01-29 14:47:37 +0300

    config react-router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,53 +13,78 @@ import AllJobs from "./pages/dashboard/AllJobs";
 import Profile from "./pages/dashboard/Profile";
 import SharedLayout from "./pages/dashboard/SharedLayout";
 import Stats from "./pages/dashboard/Stats";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import React, { useEffect } from "react";
-import { login } from "./features/auth/authSlice";
 
 function App() {
-  const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth); // Access the user state
+  const location = useLocation();
+  const noNavbar =
+    location.pathname === "/register" ||
+    location.pathname === "/" ||
+    location.pathname.includes("password");
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    if (storedUser) {
-      dispatch(login.fulfilled(storedUser)); // Directly fulfill the user state
-    }
-  }, [dispatch]);
-  console.log("Auth state:", { user });
   return (
-    <BrowserRouter>
-      <Routes>
-        {user && (
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Stats />} />
-            <Route path="all-jobs" element={<AllJobs />} />
-            <Route path="add-job" element={<AddJob />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        )}
-
-        {/* Public routes */}
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/activate/:uid/:token" element={<Activation />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route
-          path="/password/reset/confirm/:uid/:token"
-          element={<ResetPasswordConfirm />}
+    <>
+      {noNavbar ? (
+        <Routes>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/request/password_reset" element={<ResetPassword />} />
+          <Route
+            path="/password-reset/:token"
+            element={<ResetPasswordConfirm />}
+          />
+        </Routes>
+      ) : (
+        <SharedLayout
+          content={
+            <Routes>
+              <Route element={<ProtectedRoute />}>
+                {/* <Route path="/" element={<SharedLayout />}> */}
+                {/* <Route index element={<Stats />} /> */}
+                <Route path="all-jobs" element={<AllJobs />} />
+                <Route path="add-job" element={<AddJob />} />
+                <Route path="profile" element={<Profile />} />
+              </Route>
+            </Routes>
+          }
         />
+      )}
+    </>
 
-        {/* Catch-all for undefined routes */}
-        {/* <Route path="*" element={<Error />} /> */}
-        <Route
-          path="*"
-          element={<Navigate to={user ? "/" : "/landing"} replace={true} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    //           <Route path="/" element={<SharedLayout />}>
+    //             <Route index element={<Stats />} />
+    //             <Route path="all-jobs" element={<AllJobs />} />
+    //             <Route path="add-job" element={<AddJob />} />
+    //             <Route path="profile" element={<Profile />} />
+    //           </Route>
+    //         )}
+
+    //         {/* Public routes */}
+    //         <Route path="/landing" element={<Landing />} />
+    //         <Route path="/register" element={<Register />} />
+    //         <Route path="/login" element={<Login />} />
+    //         <Route path="/activate/:uid/:token" element={<Activation />} />
+    //         <Route path="/reset-password" element={<ResetPassword />} />
+    //         <Route
+    //           path="/password/reset/confirm/:uid/:token"
+    //           element={<ResetPasswordConfirm />}
+    //         />
+
+    //         {/* Catch-all for undefined routes */}
+    //         {/* <Route path="*" element={<Error />} /> */}
+    //         <Route
+    //           path="*"
+    //           element={<Navigate to={user ? "/" : "/landing"} replace={true} />}
+    //         />
   );
 }
 
